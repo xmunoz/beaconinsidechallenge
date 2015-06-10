141178e6c6e5460b172b4e3120c198b3763fc37e
Cristina Munoz <hi@xmunoz.com>
2015-06-10 17:55:38 -0500

    task 2 complete, but cors blocks request
    The specific error is:
    
    XMLHttpRequest cannot load https://beaconinside-crypto.appspot.com/caesar/decrypt?message=DSFD. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
    
    Setting this response header would all other apps to access the content.

--- loadCipher.js
+++ loadCipher.js
@@ -1,2 +1,16 @@
-(function() {
+(function() { 
+
+    $("button").click(function() {
+        var userInput = this.form.elements["user-input"].value.toUpperCase();
+        var operation = this.form.elements["operation"].value;
+        var apiEndpoint = "https://beaconinside-crypto.appspot.com/caesar/" + operation
+        var params = { "message": userInput }
+        $.get(apiEndpoint, params)
+            .done(function(cipherText) {
+                $(".result").text(cipherText);
+            })
+            .error(function(err) {
+                console.log(err)
+            })
+    });
 })();
